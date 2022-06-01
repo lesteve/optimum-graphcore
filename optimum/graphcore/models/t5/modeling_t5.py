63c8812d5ef75208187506bd6d9fd2c631bb5449
Michael Benayoun <mickbenayoun@gmail.com>
2022-06-01 17:58:01 +0200

    T5 fix

--- optimum/graphcore/models/t5/modeling_t5.py
+++ optimum/graphcore/models/t5/modeling_t5.py
@@ -12,7 +12,6 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
-import math
 import warnings
 
 import torch
@@ -22,7 +21,7 @@ from torch import Tensor
 import poptorch
 from optimum.utils import logging
 from transformers import T5ForConditionalGeneration
-from transformers.modeling_outputs import BaseModelOutput, Seq2SeqLMOutput
+from transformers.modeling_outputs import BaseModelOutput, BaseModelOutputWithPastAndCrossAttentions, Seq2SeqLMOutput
 from transformers.models.t5.modeling_t5 import __HEAD_MASK_WARNING_MSG, T5LayerNorm, T5Stack
 
 from ...generation_utils import IPUGenerationMixin
@@ -41,20 +40,9 @@ logger = logging.get_logger(__name__)
 
 
 class CustomT5Stack(T5Stack):
-
     def prepare_for_parallelize(self, user_id=None, ipu_id=None):
         self.user_id = user_id
         self.ipu_id = ipu_id
-        # self_attn = self.block[0].layer[0].SelfAttention
-        # self.relative_attention_bias = self_attn.relative_attention_bias
-        # self.relative_attention_num_buckets = self_attn.relative_attention_num_buckets
-        # self.relative_attention_max_distance = self_attn.relative_attention_max_distance
-        # self_attn.relative_attention_bias = None
-        # self_attn.relative_attention_num_buckets = None
-        # self_attn.relative_attention_max_distance = None
-
-
-        # self.relative_attention_biaself.block[0].layer[0].SelfAttention.relative_attention_bias
 
     def invert_attention_mask(self, encoder_attention_mask: Tensor) -> Tensor:
         if encoder_attention_mask.dim() == 3:
@@ -72,76 +60,10 @@ class CustomT5Stack(T5Stack):
         encoder_extended_attention_mask = (1.0 - encoder_extended_attention_mask) * -1e4
         return encoder_extended_attention_mask
 
-    @staticmethod
-    def _relative_position_bucket(relative_position, bidirectional=True, num_buckets=32, max_distance=128):
-        """
-        Adapted from Mesh Tensorflow:
-        https://github.com/tensorflow/mesh/blob/0cb87fe07da627bf0b7e60475d59f95ed6b5be3d/mesh_tensorflow/transformer/transformer_layers.py#L593
-
-        Translate relative position to a bucket number for relative attention. The relative position is defined as
-        memory_position - query_position, i.e. the distance in tokens from the attending position to the attended-to
-        position. If bidirectional=False, then positive relative positions are invalid. We use smaller buckets for
-        small absolute relative_position and larger buckets for larger absolute relative_positions. All relative
-        positions >=max_distance map to the same bucket. All relative positions <=-max_distance map to the same bucket.
-        This should allow for more graceful generalization to longer sequences than the model has been trained on
-
-        Args:
-            relative_position: an int32 Tensor
-            bidirectional: a boolean - whether the attention is bidirectional
-            num_buckets: an integer
-            max_distance: an integer
-
-        Returns:
-            a Tensor with the same shape as relative_position, containing int32 values in the range [0, num_buckets)
-        """
-        relative_buckets = 0
-        if bidirectional:
-            num_buckets //= 2
-            relative_buckets += (relative_position > 0).to(torch.long) * num_buckets
-            relative_position = torch.abs(relative_position)
-        else:
-            relative_position = -torch.min(relative_position, torch.zeros_like(relative_position))
-        # now relative_position is in the range [0, inf)
-
-        # half of the buckets are for exact increments in positions
-        max_exact = num_buckets // 2
-        is_small = relative_position < max_exact
-
-        # The other half of the buckets are for logarithmically bigger bins in positions up to max_distance
-        relative_position_if_large = max_exact + (
-            torch.log(relative_position.float() / max_exact)
-            / math.log(max_distance / max_exact)
-            * (num_buckets - max_exact)
-        ).to(torch.long)
-        relative_position_if_large = torch.min(
-            relative_position_if_large, torch.full_like(relative_position_if_large, num_buckets - 1)
-        )
-
-        relative_buckets += torch.where(is_small, relative_position, relative_position_if_large)
-        return relative_buckets
-
-    # def compute_bias(self, query_length, key_length, device=None):
-    #     """Compute binned relative position bias"""
-    #     if device is None:
-    #         device = self.relative_attention_bias.weight.device
-    #     context_position = torch.arange(query_length, dtype=torch.long, device=device)[:, None]
-    #     memory_position = torch.arange(key_length, dtype=torch.long, device=device)[None, :]
-    #     relative_position = memory_position - context_position  # shape (query_length, key_length)
-    #     relative_position_bucket = self._relative_position_bucket(
-    #         relative_position,  # shape (query_length, key_length)
-    #         bidirectional=(not self.is_decoder),
-    #         num_buckets=self.relative_attention_num_buckets,
-    #         max_distance=self.relative_attention_max_distance,
-    #     )
-    #     values = self.relative_attention_bias(relative_position_bucket)  # shape (query_length, key_length, num_heads)
-    #     values = values.permute([2, 0, 1]).unsqueeze(0)  # shape (1, num_heads, query_length, key_length)
-    #     return values
-
     def compute_bias(self, query_length, key_length, device=None):
         """Compute binned relative position bias"""
         self_attn = self.block[0].layer[0].SelfAttention
         if device is None:
-            # device = self.relative_attention_bias.weight.device
             device = self_attn.relative_attention_bias.weight.device
         context_position = torch.arange(query_length, dtype=torch.long, device=device)[:, None]
         memory_position = torch.arange(key_length, dtype=torch.long, device=device)[None, :]
@@ -150,13 +72,12 @@ class CustomT5Stack(T5Stack):
         relative_position_bucket = self_attn._relative_position_bucket(
             relative_position,  # shape (query_length, key_length)
             bidirectional=(not self.is_decoder),
-            # num_buckets=self.relative_attention_num_buckets,
-            # max_distance=self.relative_attention_max_distance,
             num_buckets=self_attn.relative_attention_num_buckets,
             max_distance=self_attn.relative_attention_max_distance,
         )
-        # values = self.relative_attention_bias(relative_position_bucket)  # shape (query_length, key_length, num_heads)
-        values = self_attn.relative_attention_bias(relative_position_bucket)  # shape (query_length, key_length, num_heads)
+        values = self_attn.relative_attention_bias(
+            relative_position_bucket
+        )  # shape (query_length, key_length, num_heads)
         values = values.permute([2, 0, 1]).unsqueeze(0)  # shape (1, num_heads, query_length, key_length)
         return values
 
@@ -223,7 +144,9 @@ class CustomT5Stack(T5Stack):
 
             # We can provide a self-attention mask of dimensions [batch_size, from_seq_length, to_seq_length]
             # ourselves in which case we just need to make it broadcastable to all heads.
-            extended_attention_mask = self.get_extended_attention_mask(attention_mask, input_shape, device=attention_mask.device)
+            extended_attention_mask = self.get_extended_attention_mask(
+                attention_mask, input_shape, device=attention_mask.device
+            )
 
             # If a 2D or 3D attention mask is provided for the cross-attention
             # we need to make broadcastable to [batch_size, num_heads, seq_length, seq_length]
@@ -255,7 +178,9 @@ class CustomT5Stack(T5Stack):
                 # if past_key_values is not None:
                 #     position_bias = position_bias[:, :, -hidden_states.size(1) :, :]
 
-                position_bias = position_bias + extended_attention_mask  # (batch_size, n_heads, seq_length, key_length)
+                position_bias = (
+                    position_bias + extended_attention_mask
+                )  # (batch_size, n_heads, seq_length, key_length)
 
             encoder_decoder_position_bias = None
             if self.is_decoder and encoder_hidden_states is not None:
@@ -294,9 +219,7 @@ class CustomT5Stack(T5Stack):
             # We share the position biases between the layers - the first layer store them
             # layer_outputs = hidden-states, key-value-states (self-attention position bias), (self-attention weights),
             # (cross-attention position bias), (cross-attention weights)
-            # position_bias = layer_outputs[2]
-            # if self.is_decoder and encoder_hidden_states is not None:
-            #     encoder_decoder_position_bias = layer_outputs[4 if output_attentions else 3]
+
             # append next layer key value states
             if use_cache:
                 present_key_value_states = present_key_value_states + (present_key_value_state,)
